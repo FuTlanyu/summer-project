f287f5931ca6c0bc9d9b19364da2fd9d3d788a69
futy <36615950+FuTlanyu@users.noreply.github.com>
2022-09-21 10:36:00 +0800

    Update SegNet.py

--- SegNet.py
+++ SegNet.py
@@ -1,12 +1,7 @@
 '''
 Adapted from https://github.com/vinceecws/SegNet_PyTorch
 '''
-
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import pytorch_model_summary as pms
-
+from utils import *
 
 class SegNet(nn.Module):
     def block(self, conv_num, in_chn, out_chn):
@@ -34,11 +29,12 @@ class SegNet(nn.Module):
         
         return nn.Sequential(*layers)
 
-    def __init__(self, bayes=False, dropout=0.5, classes=2):
+    def __init__(self, bayes, dropout, dropout_state, classes=2):
         super(SegNet, self).__init__()
         self.classes = classes
         self.bayes = bayes
         self.dropout = dropout
+        self.dropout_state = dropout_state
 
         if self.bayes:
             self.dropout = nn.Dropout(p=self.dropout)
@@ -78,41 +74,41 @@ class SegNet(nn.Module):
         x = self.conv_block3(x)
         size3 = x.size()
         x, ind3 = self.maxEn(x)
-        # dropout 3
-        if self.bayes:
+        # dropout 1_1
+        if self.bayes and self.dropout_state[0]:
             x = self.dropout(x)
         
         x = self.conv_block4(x)
         size4 = x.size()
         x, ind4 = self.maxEn(x)
-        # dropout 4
-        if self.bayes:
+        # dropout 2_1
+        if self.bayes and self.dropout_state[1]:
             x = self.dropout(x)
 
         x = self.conv_block5(x)
         size5 = x.size()
         x, ind5 = self.maxEn(x)
-        # dropout 5
-        if self.bayes:
+        # dropout 3_1
+        if self.bayes and self.dropout_state[2]:
             x = self.dropout(x)
 
         # Decoder
         x = self.maxDe(x, ind5, output_size=size5)
         x = self.deconv_block1(x)
-        # dropout 6
-        if self.bayes:
+        # dropout 3_2
+        if self.bayes and self.dropout_state[2]:
             x = self.dropout(x)
 
         x = self.maxDe(x, ind4, output_size=size4)
         x = self.deconv_block2(x)
-        # dropout 7
-        if self.bayes:
+        # dropout 2_2
+        if self.bayes and self.dropout_state[1]:
             x = self.dropout(x)
 
         x = self.maxDe(x, ind3, output_size=size3)
         x = self.deconv_block3(x)
-        # dropout 8
-        if self.bayes:
+        # dropout 1_2
+        if self.bayes and self.dropout_state[0]:
             x = self.dropout(x)
 
         x = self.maxDe(x, ind2, output_size=size2)
@@ -121,11 +117,9 @@ class SegNet(nn.Module):
         x = self.maxDe(x, ind1, output_size=size1)
         x = self.deconv_block5(x)
 
-        # x = F.softmax(x, dim=1)
-
         return x
 
-print(pms.summary(SegNet(classes=2), torch.zeros((1, 3, 256, 256)), show_input=True, show_hierarchical=False))
+# print(pms.summary(SegNet(classes=2), torch.zeros((1, 3, 256, 256)), show_input=True, show_hierarchical=False))
 # m = SegNet(bayes=True)
 # param = list(m.parameters())
 # print()
