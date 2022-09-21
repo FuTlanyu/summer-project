d24ff456c43b2a5ca6fdf648dad893b493f31f94
futy <36615950+FuTlanyu@users.noreply.github.com>
2022-09-21 10:25:02 +0800

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,30 +1,20 @@
-import os
-import random
-
 from utils import *
-from os.path import join
-import numpy as np
-import torch
-import torch.nn as nn
-import torchvision.transforms as T
-import torch.nn.functional as F
-from torchvision.transforms import functional as TF
-
-from torch.utils.data import Dataset
-import h5py
-
 
 '''
 Dataset for segmentation
 '''
 class AFDataset(Dataset):
-    def __init__(self, type, augmentation = False):
+    def __init__(self, type, augmentation, aug_list=None, fid=None):
         # open the h5 file
         assert type == 'train' or 'val' or 'test', 'Unrecognizable dataset type'
         self.type = type
-        h5_file_name = join(SEG_H5_FILE_PATH, f'{self.type}.h5')
+        if type =='test':
+            h5_file_name = join(SEG_H5_FILE_PATH, f'{self.type}{fid}.h5')
+        else:
+            h5_file_name = join(SEG_H5_FILE_PATH, f'{self.type}.h5')
         self.hdf5 = h5py.File(h5_file_name, 'r')
         self.augmentation = augmentation
+        self.aug_list = aug_list
         if self.type != 'train':
             self.file_keys = list(self.hdf5.keys())
             # filenames = [self.file_keys[i] for i in range(0, len(self.file_keys), 2)]
@@ -44,44 +34,68 @@ class AFDataset(Dataset):
         if self.type == 'train':        
             img = self.hdf5['imgs'][index]
             mask = self.hdf5['masks'][index]
+
             
             if self.augmentation:
-                img, mask = self.augment(img, mask) 
+                img, mask = self.augment(img, mask, self.aug_list) 
 
         else:
             filename = self.file_keys[2*index]
+            
             img = self.hdf5[filename][0]
             mask = self.hdf5[filename][1]
             mask_origin = self.hdf5[filename+'_origin'][:,:,-1]
+            # # test
+            # if filename == '1564100_OD_2016':
+            #     mask_show = mask*255
+            #     mask_origin_show = mask_origin*255
+            #     cv2.imshow('mask_show', mask_show)
+            #     cv2.imshow('mask_origin_show', mask_origin_show)
+            #     print()
+
         
-        # transform to tensor
+        # Transform to tensor
         img = TF.to_tensor(img)
         mask = torch.tensor(np.array(mask)[:,:,-1], dtype=torch.long)
-
-        normalise = T.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        # Normalisation
+        normalise = transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
         img = normalise(img)
 
         return img, mask, mask_origin, filename
 
-    def augment(self, img, mask):
+    def augment(self, img, mask, aug_list):
         # transform to pillow image
         img = TF.to_pil_image(img)
         mask = TF.to_pil_image(mask)
 
-        # # random rotation
-        # deg = T.RandomRotation.get_params((0,180))
-        # img = TF.rotate(img, deg)
-        # mask = TF.rotate(mask, deg)
+        if 'crop_rand' in aug_list:
+            # random crop
+            i, j, h, w = transforms.RandomCrop.get_params(img, output_size=(256, 256))
+            img = TF.crop(img, i, j, h, w)
+            mask = TF.crop(mask, i, j, h, w)
+        elif 'crop_stride' in aug_list:
+            randid = np.random.randint(25)
+            i = 128*(randid//5)
+            j = 128*(randid%5)
+            img = TF.crop(img, i, j, 256, 256)
+            mask = TF.crop(mask, i, j, 256, 256)
 
-        # random horizontal flipping
-        if random.random() > 0.5:
-            img = TF.hflip(img)
-            mask = TF.hflip(mask)
 
-        # random vertical flipping
-        if random.random() > 0.5:
-            img = TF.vflip(img)
-            mask = TF.vflip(mask)
+        if 'flip' in aug_list:
+            # random horizontal flipping
+            if random.random() > 0.5:
+                img = TF.hflip(img)
+                mask = TF.hflip(mask)
+            # random vertical flipping
+            if random.random() > 0.5:
+                img = TF.vflip(img)
+                mask = TF.vflip(mask)
+
+        if 'rotate' in aug_list:
+            # random rotation
+            deg = transforms.RandomRotation.get_params([-5,5])
+            img = TF.rotate(img, deg)
+            mask = TF.rotate(mask, deg)
 
         return img, mask
 
@@ -95,10 +109,14 @@ class AFDataset(Dataset):
 Dataset for classification
 '''
 class ASDataset(Dataset):
-    def __init__(self, type, transform):
+    def __init__(self, type, transform, fid=None):
         # open the h5 file
         assert type == 'train' or 'val' or 'test', 'Unrecognizable dataset type'
-        h5_file_name = join(CLA_H5_FILE_PATH, f'{type}.h5')
+
+        if type =='test':
+            h5_file_name = join(CLA_H5_FILE_PATH, f'{type}{fid}.h5')
+        else:
+            h5_file_name = join(CLA_H5_FILE_PATH, f'{type}.h5')
         self.dset = h5py.File(h5_file_name, 'r')
         self.length = self.dset['labels'].shape[0]
         
@@ -121,7 +139,3 @@ class ASDataset(Dataset):
         self.dset.close()
 
 
-
-'''
-Dataset for multi-task learning
-'''
